e2845d8f48f1662080102e644fff4e123ab9394e
Entrar <Entrar@PC-1027>
2017-09-28 09:47:25 +0100

    Cambiar colores RGB

---
+++ src/Estela.java
@@ -0,0 +1,21 @@
+
+import java.awt.Point;
+
+public class Estela {
+    
+    private Point[] points;
+    private String forma;
+
+    
+    public Estela(){
+        points = new Point[5];
+        forma = "Círculo";
+        
+    }
+    
+    public void setForma(String forma) {
+        this.forma = forma;
+    }
+    
+    
+}
